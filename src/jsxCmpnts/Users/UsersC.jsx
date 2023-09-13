d18ee216cc243562f9e2b1bed394267de25dbd25
Kalique <kalique@gmail.com>
2023-09-13 19:59:08 +0200

    first class component added

--- src/jsxCmpnts/Users/UsersC.jsx
+++ src/jsxCmpnts/Users/UsersC.jsx
@@ -4,16 +4,18 @@ import classes from "./Users.module.css";
 import axios from "axios";
 
 class Users extends React.Component {
-
-    componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            this.props.setUsers(response.data.items)
-        })
+    getUsers = () => {
+        if (this.props.usersData.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                this.props.setUsers(response.data.items)
+            })
+        }
     }
-
     render() {
         return <div className={classes.user}>
-            {this.props.usersData.map(user => <div key={user.id}>
+            <button onClick={this.getUsers}>Get Users</button>
+            {
+                this.props.usersData.map(user => <div key={user.id}>
 
                 <span>
                     <div>
@@ -29,7 +31,7 @@ class Users extends React.Component {
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{user.name}</div><div>{user.status}</div>
                     </span>
@@ -37,7 +39,7 @@ class Users extends React.Component {
                         <div>{'user.location.country'}</div><div>{'user.location.city'}</div>
                     </span>
                 </span>
-            </div>)
+                </div>)
             }
         </div>
     }
